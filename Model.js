5df8a4e1ad2ada29bc915a6e88874a19d295b4ec
soham9017 <120491760+soham9017@users.noreply.github.com>
2023-01-03 20:21:27 +0530

    Adding Starting Project

---
+++ Model.js
@@ -0,0 +1,36 @@
+/*var button=document.getElementsByTagName("button")[0];
+button.addEventListener("mouseenter", function(){
+	console.log("CLICK!!");
+
+	});*/
+
+var button= document.getElementById("enter");
+var input= document.getElementById("userinput");
+var ul=document.querySelector("ul");
+
+function inputLength(){
+	return input.value.length;
+}
+
+function createListElement(){
+var li=document.createElement("li");
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+    input.value="";
+
+}
+
+function addListAfterClick(){
+	if(inputLength()>0){
+    createListElement();
+     }
+}
+
+ function addListAfterKeypress(event){
+	if(inputLength()>0 && event.which===13){
+       createListElement();
+     }
+}
+
+button.addEventListener("click", addListAfterClick);
+input.addEventListener("keypress", addListAfterKeypress);
